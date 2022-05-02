3c87091c4c6544d7d900b86b040c1039ff6b9886
Mauriceia <mauriceiaeua@gmail.com>
2022-05-01 21:32:30 -0400

    Add project files.

---
+++ Python_ProjectOne/Python_ProjectOne.py
@@ -0,0 +1,40 @@
+#Story time madlib age: 3+  Kids criation
+
+animalOne = input("funny animal: ")
+verbOne = input("Verb: ")
+adjOne = input("Adjective: ")
+noun = input("Name: ")
+animalPet = input("Pet: ")
+adjTwo = input("Adjective: ")
+smallAnimal = input("Small animal: ")
+bigAnimal = input("Big Animal: ")
+nounTwo = input ("Summer vacation spot: ")
+nounthree = input("Shower item: ")
+oneActivity =  input("Outside activity: ")
+adjThree = input("Music: ")
+tallAnimal = input ("Tall animal: ")
+relaxActivity = input ("Relaxing activity: ")
+cuteAnimal = input("Cute animal: ")
+seaAnimal = input("Ocean animal: ")
+adjFour = input("Adjective: ")
+print("****************************************************************************************************************\n")
+
+madlib = f"I want a {animalOne} who scratches his head! He would {verbOne} from the lights and he would be {adjOne} on the bed!\n\
+Everyone has wanted a pet at one point or another. \n It was not different for {noun}.\n {noun} was sure that {noun} wanted a pet.\n {noun} talked to Dad about which pet to get.\n\
+I want a {animalPet} who scratches his head!\n He’d swing in the house and he’d {adjTwo} on the bed.\n Well, said Dad in a kind sort of way,\n\
+Did you know that {animalOne} eat {smallAnimal} everyday?\n Oh No, {noun} said: We have none to eat.\n Mom likes to {smallAnimal} all the bugs with her feet.\n\
+Then I want a {bigAnimal}!” {noun} screamed to Dad.\n {bigAnimal} would eat all our trash, and Mom would be glad!\n {bigAnimal} need water, it helps them stay cool.\n\
+I guess we can build him a giant sized {nounTwo}.\n No, {noun} said: he would be too big to dry\n We would need a mountain of {nounthree} to try.\n\
+I want an Otter who triple back flips,\n I would practice {oneActivity} when we go for dips.\n Did you know most otters can whistle and chirp?\n\
+They growl and snort, and perhaps even burp?\n Oh, {noun} said with a sad looking frown,\n We would have to tell him to keep the {adjThree} down\n\
+Let’s get a {tallAnimal}!” {noun} said with a smile.\n I would slide from his neck to a giant clothes pile!\n {tallAnimal} hold the record for tallest of all.\n\
+The jump from their head is a twenty foot fall!\n Gosh, {noun} said as he looked at the ceiling,\n He would bump his head even if he were {relaxActivity}.\n\
+Well, how about a {cuteAnimal}? {noun} said to Dad,\n A fluffy and floppy one can’t be that bad.\n {cuteAnimal} like eating and chew all your stuff.\n\
+Raising a {cuteAnimal} can really be tough.\n Wow! {noun} answered, Is that really true? \n Picking a pet is not easy to do.\n\
+I want a {seaAnimal}!” {noun} said, “Let’s get one!\n I think a {seaAnimal} could be lots of fun.\n Hmm... said his Dad, with a look of delight.\n\
+Good idea {noun}; I think that you’re right!\n They’re really good swimmers and make {adjFour} faces.\n\
+And if we get two, we could watch them have races!\n Okay, said his Dad, That’s just what we’ll do.\n\
+One {seaAnimal} for me, one {seaAnimal} for you!\n\
+"
+
+print(madlib)
